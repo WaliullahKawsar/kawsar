256b37e93c423982e905133da4c5c309334f8d88
WaliullahKawsar <70805297+WaliullahKawsar@users.noreply.github.com>
2023-09-23 11:33:49 +0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.playtubescript.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com
+// +------------------------------------------------------------------------+
+// | PlayTube - The Ultimate Video Sharing Platform
+// | Copyright (c) 2017 PlayTube. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "kawsar";
+// MySQL Database Password
+$sql_db_pass = "admin";
+// MySQL Database Name
+$sql_db_name = "kawsar";
+
+// Site URL
+$site_url = "http://localhost/kawsar"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "dsfhkafl9999994ew36hjfgsd"; // Your purchase code, don't give it to anyone.
+
+$auto_redirect = true;
+?>
